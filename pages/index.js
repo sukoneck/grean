a1d03d45ae6c515106f1d663a1f524d42ba47098
sukoneck <19413126+sukoneck@users.noreply.github.com>
2021-10-30 13:36:01 -0700

    clean up for mobile

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
       <Head>
         <title>grean</title>
         <meta name="description" content="an 🦙🍜™ project" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌿</text></svg>"></link>
       </Head>
 
@@ -28,8 +27,6 @@ export default function Home() {
           <Image 
             src="/images/pipe.gif" 
             alt="grean" 
-            // width={624} 
-            // height={332} 
             width={489} 
             height={489} 
           />
